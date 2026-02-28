3e56810ba4fbc3e17663c9fff1cc7c2ac32bfd24
minhhjj5678 <83084666+minhhjj5678@users.noreply.github.com>
2026-02-28 15:55:21 +0000

    Refactor CapabilityEventHandler to improve player capability management and remove commented-out villager tick logic

--- src/main/java/com/minhhjjj/efprogressivediff/event/CapabilityEventHandler.java
+++ src/main/java/com/minhhjjj/efprogressivediff/event/CapabilityEventHandler.java
@@ -5,6 +5,7 @@ import com.minhhjjj.efprogressivediff.capability.PlayerDataCapability;
 
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.npc.Villager;
 import net.minecraft.world.entity.player.Player;
@@ -25,20 +26,23 @@ public class CapabilityEventHandler {
             if (!player.getCapability(PlayerDataCapability.INSTANCE).isPresent()) {
                 event.addCapability(new ResourceLocation(EFProgressiveDiff.MODID, "player_diff"), new PlayerDataCapability());
             }
-        } else if (event.getObject() instanceof Villager villager) {
-            if (!villager.getCapability(PlayerDataCapability.INSTANCE).isPresent()) {
-                event.addCapability(new ResourceLocation(EFProgressiveDiff.MODID, "player_diff"), new PlayerDataCapability());
-            }
         }
+        // else if (event.getObject() instanceof Villager villager) {
+        //     if (!villager.getCapability(PlayerDataCapability.INSTANCE).isPresent()) {
+        //         event.addCapability(new ResourceLocation(EFProgressiveDiff.MODID, "player_diff"), new PlayerDataCapability());
+        //     }
+        // }
     }
 
     @SubscribeEvent
     public static void onPlayerClone(PlayerEvent.Clone event) {
+        event.getOriginal().reviveCaps();
         event.getOriginal().getCapability(PlayerDataCapability.INSTANCE).ifPresent(originalCap -> {
             event.getEntity().getCapability(PlayerDataCapability.INSTANCE).ifPresent(newCap -> {
                 newCap.copyFrom(originalCap);
             });
         });
+        event.getOriginal().invalidateCaps();
     }
 
     @SubscribeEvent
@@ -46,30 +50,30 @@ public class CapabilityEventHandler {
         if (!event.side.isServer()) return;
         if (event.phase != TickEvent.Phase.END) return;
         event.player.getCapability(PlayerDataCapability.INSTANCE).ifPresent(cap -> {
-            cap.tick();
+            cap.tick((ServerPlayer)event.player);
             cap.debug();
         });
     }
 
-    @SubscribeEvent
-    public static void onVillagerTick(LivingTickEvent event) {
-        if (event.getEntity().level().isClientSide()) return;
-        if (event.getEntity() instanceof Villager villager) {
-            villager.getCapability(PlayerDataCapability.INSTANCE).ifPresent(cap -> {
-                cap.tick();
-                cap.debug();
-            });
-        }
-    }
+    // @SubscribeEvent
+    // public static void onVillagerTick(LivingTickEvent event) {
+    //     if (event.getEntity().level().isClientSide()) return;
+    //     if (event.getEntity() instanceof Villager villager) {
+    //         villager.getCapability(PlayerDataCapability.INSTANCE).ifPresent(cap -> {
+    //             cap.tick();
+    //             cap.debug();
+    //         });
+    //     }
+    // }
 
-    @SuppressWarnings("null")
-    @SubscribeEvent
-    public static void onEntityInteract(PlayerInteractEvent.EntityInteract event) {
-        if (event.getTarget() instanceof Villager villager) {
-            if (villager.level().isClientSide()) return;
-            villager.getCapability(PlayerDataCapability.INSTANCE).ifPresent(villagerCap -> {
-                event.getEntity().sendSystemMessage(Component.literal("Dân làng tên " + villager.getName() + " đang có độ khó " + villagerCap.getDifficulty()));
-            });
-        }
-    }
+    // @SuppressWarnings("null")
+    // @SubscribeEvent
+    // public static void onEntityInteract(PlayerInteractEvent.EntityInteract event) {
+    //     if (event.getTarget() instanceof Villager villager) {
+    //         if (villager.level().isClientSide()) return;
+    //         villager.getCapability(PlayerDataCapability.INSTANCE).ifPresent(villagerCap -> {
+    //             event.getEntity().sendSystemMessage(Component.literal("Dân làng tên " + villager.getName() + " đang có độ khó " + villagerCap.getDifficulty()));
+    //         });
+    //     }
+    // }
 }
