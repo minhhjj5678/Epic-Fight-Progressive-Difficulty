dea2c75a0d8b3516b4e7bce0358b0475df68256a
minhhjj5678 <83084666+minhhjj5678@users.noreply.github.com>
2026-02-28 12:50:15 +0000

    Refactor EFProgressiveDiff to register capabilities and overlays, and update config registration

--- src/main/java/com/minhhjjj/efprogressivediff/EFProgressiveDiff.java
+++ src/main/java/com/minhhjjj/efprogressivediff/EFProgressiveDiff.java
@@ -1,12 +1,14 @@
 package com.minhhjjj.efprogressivediff;
 
 import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.RegisterGuiOverlaysEvent;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.common.capabilities.RegisterCapabilitiesEvent;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
 import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
@@ -14,6 +16,8 @@ import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 import com.minhhjjj.efprogressivediff.capability.PlayerDataCapability;
+import com.minhhjjj.efprogressivediff.client.DifficultyOverlay;
+import com.minhhjjj.efprogressivediff.config.PDClientConfig;
 import com.minhhjjj.efprogressivediff.config.PDConfig;
 
 @Mod(EFProgressiveDiff.MODID)
@@ -21,12 +25,15 @@ public class EFProgressiveDiff
 {
     public static final String MODID = "efprogressivediff";
 
-    public EFProgressiveDiff(FMLJavaModLoadingContext context) {
+    @SuppressWarnings("removal")
+	public EFProgressiveDiff(FMLJavaModLoadingContext context) {
         IEventBus modEventBus = context.getModEventBus();
         modEventBus.addListener(this::commonSetup);
-        MinecraftForge.EVENT_BUS.register(this);
         modEventBus.addListener(this::addCreative);
-        context.registerConfig(ModConfig.Type.COMMON, PDConfig.SPEC);
+        modEventBus.addListener(this::onRegisterCapabilities);
+        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, PDConfig.SPEC);
+        ModLoadingContext.get().registerConfig(ModConfig.Type.CLIENT, PDClientConfig.SPEC);
+        MinecraftForge.EVENT_BUS.register(this);
     }
 
     private void commonSetup(final FMLCommonSetupEvent event)
@@ -45,7 +52,6 @@ public class EFProgressiveDiff
        
     }
 
-    @SubscribeEvent
     public void onRegisterCapabilities(RegisterCapabilitiesEvent event) {
         event.register(PlayerDataCapability.class);
     }
@@ -57,6 +63,12 @@ public class EFProgressiveDiff
         public static void onClientSetup(FMLClientSetupEvent event)
         {
            
+        }
+
+        @SuppressWarnings("null")
+        @SubscribeEvent
+        public static void registerOverlays(RegisterGuiOverlaysEvent event) {
+            event.registerAboveAll(MODID, DifficultyOverlay.INSTANCE);
         }
     }
 }
